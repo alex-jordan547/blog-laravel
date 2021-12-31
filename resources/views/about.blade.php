78a9a9247969fcdf44d851b76c8f12065317b6e4
alex jordan <alextsague98@mail.com>
2021-12-31 12:54:43 +0100

    Add About Us and all link

---
+++ resources/views/about.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>About Us | Blog</title>
+
+
+</head>
+<body class="antialiased">
+
+    <p>Built with &hearts; by Alex Jordan.</p>
+    <a href="/"> Revenir à la homepage</a>
+<footer>
+    <p>&copy; Copyright {{ date('Y') }} &middot; <a href="/about-us">About Us</a></p>
+</footer>
+
+
+</body>
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Blog</title>
+        <title>Home | Blog</title>
 
 
     </head>
@@ -15,7 +15,7 @@
         <p>It's currently {{ date('H:i A')}} .</p>
 
     <footer>
-        <p>&copy; Copyright {{ date('Y') }} &middot; <a href="#">About Us</a></p>
+        <p>&copy; Copyright {{ date('Y') }} &middot; <a href="about-us">About Us</a></p>
     </footer>
 
 
