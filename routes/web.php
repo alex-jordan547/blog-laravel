75c388e67334b5d69bc89c0a53f7193f2d14e520
alex jordan <alextsague98@mail.com>
2021-12-31 19:44:38 +0100

    Differentes techniques

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,11 @@ Route::name('app_home')->get('/', function () {
     return view('Pages/home');
 });
 
+//ou on peut faire de cette façon
+//Route::get('/', 'Pages/home')->name('app_home');
+//Route::get('/about-us', 'Pages/about')->name('app_about');
+
+
 Route::name('app_about')->get('/about-us', function () {
     return view('Pages/about');
 });
